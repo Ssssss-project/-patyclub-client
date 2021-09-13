ec6f9ebc9a919e033a88d382fdec20340581f17e
xiouchin32 <xiouchin32@gmail.com>
2021-09-13 22:40:52 +0800

    add scss

--- src/components/LoginPage.vue
+++ src/components/LoginPage.vue
@@ -1,25 +1,27 @@
 <template>
-  <div class="note" :style="note">
+  <div id="Login" class="note" :style="note">
     <div class="flexbox">
       <div class="calculator">
         <img src="../assets/PatyRabbit.png" />
         <img src="../assets/PatyClub.png" />
         <input
-          clase="input"
+          class="input"
           type="text"
           v-model="account"
           placeholder="Email/帳號"
         />
         <input
-          clase="input"
+          class="input"
           type="password"
           v-model="password"
           placeholder="密碼"
         />
-        <button class="btn">登入</button>
-        <button class="btn">註冊</button>
-        <button class="btn">已Google繼續</button>
-        <button class="btn">已Facebook繼續</button>
+        <div class="login-btn">
+          <button class="btn">登入</button>
+          <button class="btn">註冊</button>
+          <button class="btn">已Google繼續</button>
+          <button class="btn">已Facebook繼續</button>
+        </div>
       </div>
     </div>
   </div>
@@ -41,37 +43,3 @@ export default {
   },
 };
 </script>
-
-<style>
-.flexbox {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 100vw;
-  height: 100vh;
-}
-
-.calculator {
-  display: grid;
-  justify-content: center;
-  align-items: center;
-
-  border: 0.3px solid #868383;
-  background: rgba(255, 255, 255, 0.75);
-  box-shadow: 0px 0px 10px 5px rgba(0, 0, 0, 0.25);
-  border-radius: 5px;
-
-  width: 300px;
-  height: 450px;
-}
-
-.btn {
-  width: 240px;
-  height: 30px;
-}
-
-.input {
-  width: 240px;
-  height: 30px;
-}
-</style>
