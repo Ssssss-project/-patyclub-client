8d43bc7385a7652cb2cb1d43127a8c6ccae87e68
HuangYuCheng <a210595770208@gmail.com>
2021-09-19 16:19:43 +0800

    add login LoginModal add Paty Icon,Logo

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,21 +1,23 @@
 <template>
-<main id="HomePage">
-  <div class="main-bar">
-    <button class="btns">所有活動</button>
-    <button class="btns">創建活動</button>
-    <router-link to="/login">
-      <button class="btns" click="navigate">登入</button>
-    </router-link>
-  </div>
-  
-</main>
+  <main id="Home">
+    <div class="main-bar">
+      <button class="btns">所有活動</button>
+      <button class="btns">創建活動</button>
+      <button class="btns" @click="bShowModal = true">登入</button>
+    </div>
+    <LoginModal :openModal="bShowModal" @close-modal="bShowModal = false"/>
+  </main>
 </template>
 
 <script>
+import LoginModal from './LoginModal.vue'
 export default {
+  components:{
+    LoginModal
+  },
   data() {
     return {
-      message: "Welcome to Vue!",
+      bShowModal: false
     };
   },
   methods: {
@@ -23,23 +25,3 @@ export default {
   },
 };
 </script>
-
-<style>
-.main-bar {
-  position: fixed;
-  width: 100%;
-  background-color: #ebe8e5;
-  height: 50px;
-  text-align: right;
-}
-.btns {
-  height: 50px;
-  width: 100px;
-  background-color: #ebe8e5;
-  border-color: #ebe8e5;
-  border-style: none;
-}
-.btns:hover {
-  background-color: rgb(238, 184, 136);
-}
-</style>
---
+++ src/components/LoginModal.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="LoginModal">
+    <transition>
+      <div v-if="openModal">
+        <div class="overlay">
+          <div class="modal">
+            <div class="right">
+              <button @click="closeModal()">X</button>
+            </div>
+            <label class="welcome">歡迎光臨</label>
+            <img src="../assets/PatyLogo.png" />
+            <input type="text" placeholder="Email/帳號" />
+            <input type="password" laceholder="密碼" />
+            <div class="right">
+              <a class="forget" href="">忘記密碼了, 995 QwQ</a>
+            </div>
+            <button class="btn-login">登入</button>
+            <button class="btn-regist">註冊</button>
+            <p />
+            <div class="hr">
+              <span class="line"></span>
+              <span class="text">或者</span>
+              <span class="line"></span>
+            </div>
+            <div>
+              <button class="btn-google">以Google繼續</button>
+              <button class="btn-facebook">以Facebook繼續</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </transition>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    openModal: {
+      type: Boolean,
+      required: true,
+    },
+  },
+  methods: {
+    closeModal() {
+      this.$emit("close-modal");
+    },
+  },
+};
+</script>
