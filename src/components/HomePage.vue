bbf89a4abc3ee5ccbf3ac0d8f07fbd2fe98b89dd
Pascal <ro020701@gmail.com>
2021-09-20 11:22:19 +0800

    背景修正

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -5,19 +5,23 @@
       <button class="btns">創建活動</button>
       <button class="btns" @click="bShowModal = true">登入</button>
     </div>
-    <div id="body" class="note" :style="note" style="height:100vh;"></div>
+    <LoginModal :openModal="bShowModal" @close-modal="bShowModal = false"/>
   </div>
+  <div class="note" :style="note" style="height:100%"></div>
 </template>
 
 <script>
+import LoginModal from './LoginModal.vue'
 export default {
+  components:{
+    LoginModal
+  },
   data() {
     return {
       note: {
-        backgroundImage:
-          "url(" + require("../assets/backgroundImage.png") + ")",
+        backgroundImage: "url(" + require("../assets/backgroundImage.png") + ")",
       },
-      bShowModal: false,
+      bShowModal: false
     };
   },
   methods: {
