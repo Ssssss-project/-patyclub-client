de0bf2d7655d8d6e449ae45a2ae756013e2531e5
charles01270 <charles01270@gmail.com>
2021-10-18 17:50:06 +0800

    add some test code

--- src/components/TestChat.vue
+++ src/components/TestChat.vue
@@ -1,24 +1,67 @@
 <template>
-    <div>
+    <div id="TestChat" v-if="openModal">
         <p>聊天室... 嗎?</p>
-        <input type = 'text' />
+        
+        <input type = 'text' v-model="inputText"/>
+        <button
+            @click="clickSubmit()"
+        >submit</button>
+        <button
+            @click="closeModal()"
+        >close</button>
     </div>
 </template>
 
-<script>
-// import * as signalR from '@aspnet/signalr'
-// export default {
-//     setup() {
-        
-//     },
 
-//     connectHub: function () {
-//   //建立設定
-//   this.alertConnection = new signalR.HubConnectionBuilder()
-//     .configureLogging(signalR.LogLevel.Debug) //設定顯示log
-//     .withUrl('/hub/Chat').build();  //設定路由
-//   //開始連線
-//   this.alertConnection.start();
-// }
-// }
+
+<script>
+import * as signalR from '@aspnet/signalr'
+export default {
+  props: {
+    openModal: {
+      type: Boolean,
+      required: true,
+    },
+  },
+
+    data() {
+        return {
+            inputText: "123",
+            hubConnection: new signalR.HubConnectionBuilder().withUrl('https://localhost:5001/hub/test').build()
+        }
+    },
+
+    methods: {
+        connectHub: function () {
+            this.hubConnection.start().then(()=>{
+                console.log('hub connection success.')
+            }).catch(()=>{
+                console.log('hub connection failed.')
+            })
+        },
+
+        clickSubmit: function() {
+            console.log(this.inputText)
+            this.hubConnection.send('SendMessage', this.inputText).then(()=>{
+                console.log('send msg');
+            }).catch((error)=>{
+                console.log('msg send error ', error);
+            })
+        },
+
+        closeModal() {
+        this.$emit("close-modal");
+        },
+
+        listenHub() {
+            this.hubConnection.on('')
+        }
+    },
+
+    created() {
+        this.connectHub();
+    }
+
+
+}
 </script>
