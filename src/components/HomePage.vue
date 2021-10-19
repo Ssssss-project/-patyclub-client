f329815c395b8e3b170b054344bf50a8faf06c24
charles01270 <charles01270@gmail.com>
2021-10-19 17:39:06 +0800

    feat 初版聊天室

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -4,7 +4,7 @@
 
       <button
         class="btns"
-        @click="bShowChat = true"
+        @click="open('top')"
       >測試聊天室</button>
       <button class="btns">所有活動</button>
       <router-link :to="`/activityCreate`">
@@ -46,10 +46,30 @@
       :openModal="bShowModal"
       @close-modal="bShowModal = false"
     />
-    <TestChat
+    <!-- <TestChat
       :openModal="bShowChat"
       @close-modal="bShowChat = false"
-    />
+    /> -->
+    <q-dialog v-model="dialog" :position="position">
+      <q-card style="width: 350 px">
+        <q-card-section class="column items-center no-wrap">
+          <div style="width: 100%">
+            <q-chat-message
+              v-for="(item, id) in receiveMsg" :key="id"
+              :name="item.sendFrom"
+              :text="item.msg"
+              :sent="this.sendFrom==item.sendFrom"
+            />
+            <!-- <li>{{item.sendFrom}}  說  {{item.msg}}</li> -->
+          </div>
+
+
+          <q-input v-model="sendFrom" label="姓名"/>
+          <q-input v-model="text" label="訊息"/>
+          <q-btn flat round @click="clickSubmit()">發送</q-btn>
+        </q-card-section>
+      </q-card>
+    </q-dialog>
 
   </main>
 
@@ -57,11 +77,27 @@
 
 <script>
 import LoginModal from "./LoginModal.vue";
-import TestChat from "./TestChat.vue";
+// import TestChat from "./TestChat.vue";
+import { ref } from 'vue';
+import * as signalR from '@aspnet/signalr';
 export default {
+  setup() {
+        const dialog = ref(false)
+        const position = ref('top')
+
+        return {
+        dialog,
+        position,
+
+        open (pos) {
+            position.value = pos
+            dialog.value = true
+        }
+        }
+  },
   components: {
     LoginModal,
-    TestChat,
+    // TestChat,
   },
   data() {
     return {
@@ -76,6 +112,14 @@ export default {
       },
       bShowModal: false,
       bShowChat: false,
+      hubConnection: new signalR.HubConnectionBuilder().withUrl('https://localhost:5001/hub/test').build(),
+      text:'',
+      sendFrom:'',
+      receiveMsg:[
+        {sendFrom: 'A', msg: ['T1']},
+        {sendFrom: 'B', msg: ['T2']},
+        {sendFrom: 'A', msg: ['T3']},
+      ]
     };
   },
   methods: {
@@ -84,6 +128,37 @@ export default {
       this.showSeconfStep = true;
     },
     routeLoginPage() {},
+    connectHub: function () {
+            this.hubConnection.start().then(()=>{
+                console.log('hub connection success.');
+                this.listenHub();
+            }).catch(()=>{
+                console.log('hub connection failed.');
+            })
+        },
+
+        clickSubmit: function() {
+            this.hubConnection.send('SendMessage', this.sendFrom , this.text).then(()=>{
+                console.log('send msg');
+            }).catch((error)=>{
+                console.log('msg send error ', error);
+            })
+        },
+
+        closeModal() {
+        this.$emit("close-modal");
+        },
+
+        listenHub() {
+            console.log('listenHub start')
+            this.hubConnection.on('SendMessageToClient', (res1, res2)=> {
+              console.log(res1, res2);
+              this.receiveMsg.push({sendFrom: res1, msg: [res2]})
+            })
+        }
   },
+    created() {
+        this.connectHub();
+    }
 };
 </script>
