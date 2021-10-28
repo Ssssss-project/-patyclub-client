f305ad291cfce09b545ece772e63bd908fc3d52b
xiouchin32 <xiouchin32@gmail.com>
2021-10-28 12:00:04 +0800

    add billboardInfo showing

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,89 +1,71 @@
 <template>
-  <main id="Home">
-    <div class="main-bar">
-
-      <button
-        class="btns"
-        @click="bShowChat = true"
-      >測試聊天室</button>
-      <button class="btns">所有活動</button>
-      <router-link :to="`/activityCreate`">
-        <button class="btns">創建活動</button>
-      </router-link>
-      <button
-        class="btns"
-        @click="bShowModal = true"
-      >登入</button>
-    </div>
-    <div class="container">
-      <transition
-        name="first-step"
-        mode="out-in"
-      >
-        <div
-          class="first-step"
-          @click="showInfo"
-          v-if="showFirstStep"
-        >
-          <img src="../assets/PatyLogo.png">
+    <main id="Home">
+        <div class="main-bar">
+            <button class="btns" @click="bShowChat = true">測試聊天室</button>
+            <button class="btns">所有活動</button>
+            <router-link :to="`/activityCreate`">
+                <button class="btns">創建活動</button>
+            </router-link>
+            <button class="btns" @click="bShowModal = true">登入</button>
         </div>
-        <div
-          class="second-step"
-          @click="showInfo"
-          v-else-if="showSeconfStep"
-        >
-          <div class="billboard">公佈欄｜BillBoard</div>
-          <div class="hotActivity">活動精選｜Hot Activities</div>
+        <div class="container">
+            <transition name="first-step" mode="out-in">
+                <div class="first-step" @click="showInfo" v-if="showFirstStep">
+                    <img src="../assets/PatyLogo.png" />
+                </div>
+                <div class="second-step" v-else-if="showSecondStep">
+                    <div class="billboard" @click="showBillBoard">公佈欄｜BillBoard</div>
+                    <div class="hotActivity" @click="showActivities">活動精選｜Hot Activities</div>
+                </div>
+                <div class="billboardInfo" v-else-if="showBillBoardInfo">
+                    <div class="billboardInfo-right" @click="showBillBoard">BillBoardInfo</div>
+                    <div class="billboardInfo-left"></div>
+                </div>
+                <div class="hotActivityInfo" @click="showActivities" v-else-if="showActivitiesInfo">ActivitiesInfo</div>
+            </transition>
         </div>
-      </transition>
-
-    </div>
-    <div
-      class="bg-image opacity-set"
-      :style="backgroundimg"
-    ></div>
-    <LoginModal
-      :openModal="bShowModal"
-      @close-modal="bShowModal = false"
-    />
-    <TestChat
-      :openModal="bShowChat"
-      @close-modal="bShowChat = false"
-    />
-
-  </main>
-
+        <div class="bg-image opacity-set" :style="backgroundimg"></div>
+        <LoginModal :openModal="bShowModal" @close-modal="bShowModal = false" />
+        <TestChat :openModal="bShowChat" @close-modal="bShowChat = false" />
+    </main>
 </template>
 
 <script>
 import LoginModal from "./LoginModal.vue";
 import TestChat from "./TestChat.vue";
 export default {
-  components: {
-    LoginModal,
-    TestChat,
-  },
-  data() {
-    return {
-      showFirstStep: true,
-      showSeconfStep: false,
-      backgroundimg: {
-        backgroundImage:
-          "url(" + require("../assets/backgroundImage.png") + ")",
-        backgroundRepeat: "no-repeat",
-        backgroundAttachment: "fixed",
-        backgroundPosition: "bottom",
-      },
-      bShowModal: false,
-      bShowChat: false,
-    };
-  },
-  methods: {
-    showInfo() {
-      this.showFirstStep = false;
-      this.showSeconfStep = true;
+    components: {
+        LoginModal,
+        TestChat,
     },
-    routeLoginPage() {},
-  },
+    data() {
+        return {
+            showFirstStep: true,
+            showSecondStep: false,
+            showThirdStep: false,
+            showBillBoardInfo: false,
+            showActivitiesInfo: false,
+            // backgroundimg: {
+            //   backgroundImage:
+            //     "url(" + require("../assets/backgroundImage.png") + ")",
+            //   backgroundRepeat: "no-repeat",
+            //   backgroundAttachment: "fixed",
+            //   backgroundPosition: "bottom",
+            // },
+            bShowModal: false,
+            bShowChat: false,
+        };
+    },
+    methods: {
+        showInfo() {
+            this.showFirstStep = false;
+            this.showSecondStep = true;
+        },
+        showBillBoard() {
+            this.showSecondStep = false;
+            this.showBillBoardInfo = true;
+        },
+        routeLoginPage() {},
+    },
 };
 </script>
