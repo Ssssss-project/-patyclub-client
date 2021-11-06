a8556e8b1b08d17d3176a3a753c93e95f1cac0a1
Pascal <ro020701@gmail.com>
2021-11-06 23:50:04 +0800

    feat. add components in basicInfo page

--- src/components/ActivityCreatePage/basicInfo.vue
+++ src/components/ActivityCreatePage/basicInfo.vue
@@ -1,18 +1,67 @@
 <template>
   <div class="basicInfo">
-    <p class="page-title">活動基本資料</p>
-    <p class="activity-title">活動名稱</p>
-    <q-input v-model="eventIntroduction" class="input-title" @update:modelValue="event => getitle(event)"   borderless placeholder="30字以內"/>
+    <div class="page-title">活動基本資料</div>
+    <div class="text-title">活動名稱</div>
+    <q-input v-model="eventIntroduction" class="input-title" @update:modelValue="event => getitle(event)"  borderless placeholder="30字以內"/>
+    <div class="text-category">活動類別</div>
+    <q-select class="select-category" v-model="eventCategory" :options="options" borderless/>
+    <div class="row">
+      <div class="column">
+        <div class="text-date">活動日期</div>
+        <div class="select-date">
+          <q-input v-model="eventDate" borderless>
+            <template v-slot:append>
+              <q-icon name="event" class="cursor-pointer">
+                <q-popup-proxy ref="qDateProxy" transition-show="scale" transition-hide="scale">
+                  <q-date v-model="eventDate" color="yellow-9">
+                    <div class="row items-center justify-end">
+                      <q-btn v-close-popup label="Close" color="#deb06b" flat />
+                    </div>
+                  </q-date>
+                </q-popup-proxy>
+              </q-icon>
+            </template>
+          </q-input>
+        </div>
+      </div>
+      <div class="column">
+        <div class="text-field">其他附件</div>
+        <div class="file-field">
+          <q-file bottom-slots v-model="dataField" counter max-files="12" borderless>
+            <template v-slot:append>
+              <q-icon v-if="dataField !== null" name="close" @click="dataField = null" class="cursor-pointer" />
+              <q-icon name="create_new_folder" @click.stop />
+            </template>
+            <template v-slot:hint />
+          </q-file>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script setup>
 import { ref } from "vue";
 
+// const variable
 const eventIntroduction = ref("");
+const options = ['Google', 'Facebook', 'Twitter', 'Apple', 'Oracle'];
+const eventCategory = ref("");
+const eventDate = ref("");
+const dataField = ref(null);
 
+
+// set now date
+let newdate = new Date();
+let year = newdate.getFullYear();
+let month = newdate.getMonth() + 1 < 10? "0" + (newdate.getMonth() + 1): newdate.getMonth() + 1;
+let date  = newdate.getDate() < 10? "0" + newdate.getDate(): newdate.getDate();
+eventDate.value =  year + "/" + month + "/" + date;
+
+
+// methods
 function getitle(event){
-  this.$emit('get-title', {
+  this.$emit("get-title", {
     event: event
   })
 }
--- src/views/ActivityCreate.vue
+++ src/views/ActivityCreate.vue
@@ -1,44 +1,52 @@
 <template>
   <main id="ActivityCreate">
+    <div class="main-bar">
+      <button class="btns" @click="bShowChat = true">測試聊天室</button>
+      <button class="btns">所有活動</button>
+      <router-link :to="`/activityCreate`">
+          <button class="btns">創建活動</button>
+      </router-link>
+      <button class="btns" @click="openLoginDialog">登入</button>
+    </div>
     <div class="mainContainer">
       <q-stepper class="no-shadow" v-model="step" ref="stepper" alternative-labels animated active-color="themeColor" done-color="completed">
-        <q-step :name="1" title="活動基本資料" icon="settings" :done="step > 1">
+        <q-step :name="1" class="childComponent" title="活動基本資料" icon="settings" :done="step > 1">
           <basicInfo @get-title="getTitle"></basicInfo>
         </q-step>
 
-        <q-step :name="2" title="報名表設計" icon="assignment" :done="step > 2">
+        <q-step :name="2" class="childComponent" title="報名表設計" icon="assignment" :done="step > 2">
           <formDesign></formDesign>
         </q-step>
 
-        <q-step :name="3" title="完成" icon="thumb_up_off_alt" :done="step > 3">
+        <q-step :name="3" class="childComponent" title="完成" icon="thumb_up_off_alt" :done="step > 3">
           <designCompleted></designCompleted>
         </q-step>
 
-        <q-step :name="4" title="預覽" icon="visibility" :done="step > 4">
+        <q-step :name="4" class="childComponent" title="預覽" icon="visibility" :done="step > 4">
           <preview></preview>
         </q-step>
 
-        <q-step :name="5" title="送出申請" icon="add_comment" :done="step > 5" disable>
+        <q-step :name="5" class="childComponent" title="送出申請" icon="add_comment" :done="step > 5" disable>
         </q-step>
 
-        <q-step :name="6" title="審核中" icon="preview" :done="step > 6">
+        <q-step :name="6" class="childComponent" title="審核中" icon="preview" :done="step > 6">
           <review></review>
         </q-step>
 
-        <q-step :name="7" title="審核通過" icon="fact_check" :done="step > 7">
+        <q-step :name="7" class="childComponent" title="審核通過" icon="fact_check" :done="step > 7">
           <reviewPass></reviewPass>
         </q-step>
 
-        <q-step :name="8" title="發布" icon="send" :done="step > 8">
+        <q-step :name="8" class="childComponent" title="發布" icon="send" :done="step > 8">
           <release></release>
         </q-step>
 
         <template v-slot:navigation>
           <q-stepper-navigation>
-            <q-btn class="btn-next-step" @click="$refs.stepper.next()" :label="step === 8 ? 'Finish' : 'Continue'" flat></q-btn>
-            <q-btn v-if="step > 1" flat @click="$refs.stepper.previous()" label="Back" class="q-ml-sm"></q-btn>
-            <br /><br />
-            <q-btn class="btn-next-step" @click="save">儲存</q-btn>
+            <q-btn class="btn-step" @click="$refs.stepper.next()" :label="step === 8 ? 'Finish' : 'Continue'" flat></q-btn>
+            <q-btn v-if="step > 1" flat @click="$refs.stepper.previous()" label="Back" class="btn-step" style="margin-left:5px;"></q-btn>
+            <br/>
+            <q-btn class="btn-step" style="margin-top:5px;" @click="save">儲存</q-btn>
           </q-stepper-navigation>
         </template>
 
