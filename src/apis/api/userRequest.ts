8511cea6efcddccefdb7779a30ab35c649d75384
xiouchin32 <xiouchin32@gmail.com>
2021-11-12 15:37:30 +0800

    Revert "add activity api ref #4"
    This reverts commit a9771bf9b73328a9ed3294614673ed11e7ced0eb.

--- src/apis/api/userRequest.ts
+++ src/apis/api/userRequest.ts
@@ -1,7 +1,7 @@
 import request from "../https";
 
 // 註冊
-export const apiPostRegister = (params: any) => request("post", "/Register", params);
+export const apiPostRegister = (params :any) => request("post", "/Register", params);
 
 // 取得登入結果
 export const apiGetLoginResult = (params: any) => request("get", "/Login", params);
@@ -11,6 +11,3 @@ export const apiGetAllUser = () => request("get", "/GetEntityList/getAllUser");
 
 // 取得活躍使用者
 export const apiGetActiveUser = () => request("get", "/GetEntityList/getActiveUser");
-
-//取得精選活動
-export const apiGetActive = () => request("get", "/Event/getSpecialEvent");
