ce8054368bc4c5609302c024251f12505a52d09c
xiouchin32 <xiouchin32@gmail.com>
2021-11-18 15:03:50 +0800

    add fake data about card and card-list css

--- src/views/ActivityView.vue
+++ src/views/ActivityView.vue
@@ -1,16 +1,111 @@
 <template>
-  <main id="ActivityView">
-    <Head />
-  </main>
+    <main id="ActivityView">
+        <Head />
+        <div class="container">
+            <div class="card-list">
+                <q-card class="my-card">
+                    <img src="https://cdn.quasar.dev/img/mountains.jpg" />
+                    <div color="primary" class="absolute">
+                        <span>Time</span>
+                    </div>
+                    <q-card-section>
+                        <div class="text-h6">Our Changing Planet</div>
+                        <div class="text-subtitle2">by John Doe</div>
+                    </q-card-section>
+
+                    <q-card-section class="q-pt-none">
+                        {{ lorem }}
+                    </q-card-section>
+                </q-card>
+                <q-card class="my-card">
+                    <img src="https://cdn.quasar.dev/img/mountains.jpg" />
+                    <div color="primary" class="absolute">
+                        <span>Time</span>
+                    </div>
+                    <q-card-section>
+                        <div class="text-h6">Our Changing Planet</div>
+                        <div class="text-subtitle2">by John Doe</div>
+                    </q-card-section>
+
+                    <q-card-section class="q-pt-none">
+                        {{ lorem }}
+                    </q-card-section>
+                </q-card>
+                <q-card class="my-card">
+                    <img src="https://cdn.quasar.dev/img/mountains.jpg" />
+                    <div color="primary" class="absolute">
+                        <span>Time</span>
+                    </div>
+                    <q-card-section>
+                        <div class="text-h6">Our Changing Planet</div>
+                        <div class="text-subtitle2">by John Doe</div>
+                    </q-card-section>
+
+                    <q-card-section class="q-pt-none">
+                        {{ lorem }}
+                    </q-card-section>
+                </q-card>
+                <q-card class="my-card">
+                    <img src="https://cdn.quasar.dev/img/mountains.jpg" />
+                    <div color="primary" class="absolute">
+                        <span>Time</span>
+                    </div>
+                    <q-card-section>
+                        <div class="text-h6">Our Changing Planet</div>
+                        <div class="text-subtitle2">by John Doe</div>
+                    </q-card-section>
+
+                    <q-card-section class="q-pt-none">
+                        {{ lorem }}
+                    </q-card-section>
+                </q-card>
+                <q-card class="my-card">
+                    <img src="https://cdn.quasar.dev/img/mountains.jpg" />
+                    <div color="primary" class="absolute">
+                        <span>Time</span>
+                    </div>
+                    <q-card-section>
+                        <div class="text-h6">Our Changing Planet</div>
+                        <div class="text-subtitle2">by John Doe</div>
+                    </q-card-section>
+
+                    <q-card-section class="q-pt-none">
+                        {{ lorem }}
+                    </q-card-section>
+                </q-card>
+                <q-card class="my-card">
+                    <img src="https://cdn.quasar.dev/img/mountains.jpg" />
+                    <div color="primary" class="absolute">
+                        <span>Time</span>
+                    </div>
+                    <q-card-section>
+                        <div class="text-h6">Our Changing Planet</div>
+                        <div class="text-subtitle2">by John Doe</div>
+                    </q-card-section>
+
+                    <q-card-section class="q-pt-none">
+                        {{ lorem }}
+                    </q-card-section>
+                </q-card>
+            </div>
+        </div>
+    </main>
 </template>
 
 <script>
 import Head from "../components/Head.vue";
+import { ref } from "vue";
 
 export default {
-  setup() {},
-  components: {
-    Head,
-  },
+    setup() {
+        return {
+            expanded: ref(false),
+            lorem:
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        };
+    },
+    components: {
+        Head,
+    },
 };
 </script>
