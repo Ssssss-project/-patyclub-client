77c6e7fe94b2f6d5b614dfe3094003f493dee936
xiouchin32 <xiouchin32@gmail.com>
2021-12-13 11:46:02 +0800

    add filter btn and select

--- src/views/ActivityView.vue
+++ src/views/ActivityView.vue
@@ -1,13 +1,53 @@
 <template>
+    <Head />
     <main id="ActivityView">
-        <Head />
         <div class="container">
             <div class="activity-main">
-                <div class="event-category">
-                    <event-category />
+                <div class="activity-filter">
+                    <div class="right-btn">
+                        <q-btn
+                            style="background: #eeb888; color: #402e32;border:1px solid black;"
+                            label="全部"
+                            size="sm"
+                        />
+                        <q-btn
+                            style="background: #ffffff; color: #402e32 ;border:1px solid black;"
+                            label="精選"
+                            size="sm"
+                        />
+                    </div>
+                    <div class="left-search">
+                        <div>
+                            <label>檢視</label>
+                            <q-select
+                                class="select-category"
+                                v-model="model"
+                                :options="options"
+                                borderless
+                                dense
+                                options-dense
+                            />
+                        </div>
+                        <div>
+                            <label>排序</label>
+                            <q-select
+                                class="select-category"
+                                v-model="model"
+                                :options="options"
+                                borderless
+                                dense
+                                options-dense
+                            />
+                        </div>
+                    </div>
                 </div>
-                <div class="card-list scrollbarCol">
-                    <card-list />
+                <div class="activity-information">
+                    <div class="event-category">
+                        <event-category />
+                    </div>
+                    <div class="card-list scrollbarCol">
+                        <card-list />
+                    </div>
                 </div>
             </div>
         </div>
@@ -19,8 +59,17 @@ import Head from "../components/Head.vue";
 import CardList from "../components/ActivityViewPage/CardList.vue";
 import EventCategory from "../components/ActivityViewPage/EventCategory.vue";
 
+import { ref } from "vue";
+
 export default {
-    setup() {},
+    setup() {
+        const options = ["Google", "Facebook", "Twitter", "Apple", "Oracle"];
+        const model = ref(options[0]);
+        return {
+            options,
+            model,
+        };
+    },
     components: {
         Head,
         CardList,
