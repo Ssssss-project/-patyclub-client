24b121f3348cb28398c10ac36d0fa3349a4ccae1
Pascal <ro020701@gmail.com>
2021-12-19 18:44:52 +0800

    fix activity create page

--- src/components/ActivityCreatePage/basicInfo.vue
+++ src/components/ActivityCreatePage/basicInfo.vue
@@ -2,16 +2,36 @@
   <div class="basicInfo">
     <div class="page-title">活動基本資料</div>
     <div class="text-title">活動名稱</div>
-    <q-input v-model="eventTitle" class="input-title" @update:modelValue="event => getPara('eventTitle',event)"  borderless placeholder="30字以內"/>
+    <q-input
+      v-model="eventTitle"
+      class="input-title"
+      @update:modelValue="(event) => getPara('eventTitle', event)"
+      borderless
+      placeholder="30字以內"
+    />
     <div class="row">
       <div class="column">
         <div class="text-date">活動日期</div>
         <div class="select-date">
-          <q-input v-model="eventStDate" @update:modelValue="event => getPara('eventStDate',event)"  borderless>
+          <q-input
+            v-model="eventStDate"
+            @update:modelValue="(event) => getPara('eventStDate', event)"
+            borderless
+          >
             <template v-slot:append>
               <q-icon name="event" class="cursor-pointer">
-                <q-popup-proxy ref="qDateProxy" transition-show="scale" transition-hide="scale">
-                  <q-date v-model="eventStDate" @update:modelValue="event => getPara('eventStDate',event)" color="yellow-9">
+                <q-popup-proxy
+                  ref="qDateProxy"
+                  transition-show="scale"
+                  transition-hide="scale"
+                >
+                  <q-date
+                    v-model="eventStDate"
+                    @update:modelValue="
+                      (event) => getPara('eventStDate', event)
+                    "
+                    color="yellow-9"
+                  >
                     <div class="row items-center justify-end">
                       <q-btn v-close-popup label="Close" color="#deb06b" flat />
                     </div>
@@ -25,9 +45,22 @@
       <div class="column">
         <div class="text-field">其他附件</div>
         <div class="file-field">
-          <q-file multiple append bottom-slots v-model="dataField" counter max-files="12" borderless>
+          <q-file
+            multiple
+            append
+            bottom-slots
+            v-model="dataField"
+            counter
+            max-files="12"
+            borderless
+          >
             <template v-slot:append>
-              <q-icon v-if="dataField !== null" name="close" @click="dataField = null" class="cursor-pointer" />
+              <q-icon
+                v-if="dataField !== null"
+                name="close"
+                @click="dataField = null"
+                class="cursor-pointer"
+              />
               <q-icon name="create_new_folder" @click.stop />
             </template>
             <template v-slot:hint />
@@ -36,44 +69,63 @@
       </div>
     </div>
     <div class="text-image">活動圖片</div>
-    <q-uploader class="uploader-image" :factory="factoryFn" multiple append url="https://localhost:5001/api/Event/dataUpload"/>
+    <q-uploader class="uploader-image" :factory="factoryFn" multiple append />
     <div class="text-introduction">活動簡介</div>
-    <q-input class="input-introduction" v-model="eventIntroduction" @update:modelValue="event => getPara('eventIntroduction',event)" filled type="textarea"/>
+    <q-input
+      class="input-introduction"
+      v-model="eventIntroduction"
+      @update:modelValue="(event) => getPara('eventIntroduction', event)"
+      filled
+      type="textarea"
+    />
     <div class="row">
       <div class="column">
         <div class="text-category">活動類別</div>
-        <q-select class="select-category" v-model="eventCategory" :options="options" borderless/>
+        <q-select
+          class="select-category"
+          v-model="eventCategory"
+          :options="options"
+          borderless
+        />
       </div>
       <div class="column">
         <div class="text-ageLimit">年齡限制</div>
-        <q-select class="select-ageLimit" v-model="ageLimit" :options="options" borderless/>
+        <q-select
+          class="select-ageLimit"
+          v-model="ageLimit"
+          :options="options"
+          borderless
+        />
       </div>
     </div>
     <div class="row">
       <div class="column">
         <div class="text-numberLimit">人數限制</div>
-        <q-input class="input-numberLimit" v-model="numberLimit" borderless/>
+        <q-input class="input-numberLimit" v-model="numberLimit" borderless />
       </div>
       <div class="column">
         <div class="text-cost">參與費用</div>
-        <q-input class="input-cost" v-model="cost" @update:modelValue="event => getPara('cost',event)" borderless/>
+        <q-input
+          class="input-cost"
+          v-model="cost"
+          @update:modelValue="(event) => getPara('cost', event)"
+          borderless
+        />
       </div>
     </div>
     <div class="text-addTag">增加標籤</div>
-    <q-input class="input-addTag" v-model="tag"  borderless/>
+    <q-input class="input-addTag" v-model="tag" borderless />
   </div>
-
-  <q-img :src="preview" height="300px" width="700px"/>
- 
 </template>
 
 <script setup>
 import { ref } from "vue";
-import { defineEmits } from 'vue'
+import { defineEmits } from "vue";
+import { defineProps } from "vue";
 
 // const variable
 const eventTitle = ref("");
-const options = ['Google', 'Facebook', 'Twitter', 'Apple', 'Oracle'];
+const options = ["Google", "Facebook", "Twitter", "Apple", "Oracle"];
 const eventCategory = ref("");
 const eventStDate = ref("");
 const dataField = ref(null);
@@ -82,61 +134,64 @@ const numberLimit = ref("");
 const cost = ref("");
 const tag = ref("");
 const eventIntroduction = ref("");
-const emit = defineEmits(['get-para']);
 const preview = ref(null);
 
-let savePara = {};
-// set now date
-let newdate = new Date();
-let year = newdate.getFullYear();
-let month = newdate.getMonth() + 1 < 10? "0" + (newdate.getMonth() + 1): newdate.getMonth() + 1;
-let date  = newdate.getDate() < 10? "0" + newdate.getDate(): newdate.getDate();
-eventStDate.value =  year + "/" + month + "/" + date;
-getPara('eventStDate', eventStDate.value);
+const emit = defineEmits(["get-para"]);
+const allChildPara = defineProps(["allChildPara"]);
 
+let savePara = allChildPara.allChildPara.basicInfo ? allChildPara.allChildPara.basicInfo : {};
+
+// set now date
+let newDate = new Date();
+let year = newDate.getFullYear();
+let month = newDate.getMonth() + 1 < 10 ? "0" + (newDate.getMonth() + 1) : newDate.getMonth() + 1;
+let date = newDate.getDate() < 10 ? "0" + newDate.getDate() : newDate.getDate();
+eventStDate.value = year + "/" + month + "/" + date;
+getPara("eventStDate", eventStDate.value);
 
 // methods
-function getPara(key, event){
-  switch(key){
-    case 'eventTitle':
+function getPara(key, event) {
+  switch (key) {
+    case "eventTitle":
       savePara.eventTitle = event;
       break;
 
-    case 'eventStDate':
+    case "eventStDate":
       savePara.eventStDate = event;
       break;
 
-    case 'eventIntroduction':
+    case "eventIntroduction":
       savePara.eventIntroduction = event;
       break;
 
-    case 'cost':
+    case "cost":
       savePara.cost = event;
       break;
-    
+
+    case "preview":
+      savePara.preview = event;
+      break;
+
     default:
       break;
   }
 
-  emit('get-para', {
-    event: savePara
+  emit("get-para", {
+    event: savePara,
+  });
+}
+
+function factoryFn(file) {
+  // preview.value = file[0].__img.currentSrc;
+  preview.value = file;
+  getPara("preview", preview.value);
+
+  return new Promise((resolve, reject) => {
+    resolve({
+      url: 'https://localhost:5001/api/Event/dataUpload',
+      method: 'POST',
+    })
   })
 }
-
-function factoryFn (file) {
-  // return new Promise((resolve, reject) => {
-  //   // Retrieve JWT token from your store.
-  //   const token = "myToken";
-  //   resolve({
-  //     url: 'https://localhost:5001/api/Event/dataUpload',
-  //     method: 'POST',
-  //     headers: [
-  //       { name: 'Authorization', value: `Bearer ${token}` }
-  //     ]
-  //   })
-  // })
-  preview.value = file[0].__img.currentSrc;
-}
-
 </script>
 
--- src/views/ActivityCreate.vue
+++ src/views/ActivityCreate.vue
@@ -1,43 +1,75 @@
 <template>
   <main id="ActivityCreate">
-    <div class="main-bar">
-      <button class="btns" @click="bShowChat = true">測試聊天室</button>
-      <button class="btns">所有活動</button>
-      <router-link :to="`/activityCreate`">
-          <button class="btns">創建活動</button>
-      </router-link>
-      <button class="btns" @click="openLoginDialog">登入</button>
-    </div>
+    <Head />
     <div class="mainContainer">
-      <q-stepper class="no-shadow" v-model="step" ref="stepper" alternative-labels animated active-color="themeColor" done-color="completed">
-
-        <q-step :name="1" class="childComponent" title="活動基本資料" icon="settings" :done="step > 1">
-          <basicInfo @get-para="getPara"></basicInfo>
+      <q-stepper
+        class="no-shadow"
+        v-model="step"
+        ref="stepper"
+        alternative-labels
+        animated
+        active-color="themeColor"
+        done-color="completed"
+      >
+        <q-step
+          :name="1"
+          class="childComponent"
+          title="活動基本資料"
+          icon="settings"
+          :done="step > 1"
+        >
+          <basicInfo :allChildPara="allChildPara" @get-para="getPara" />
         </q-step>
 
-        <q-step :name="2" class="childComponent" title="報名表設計" icon="assignment" :done="step > 2">
-          <formDesign></formDesign>
+        <q-step
+          :name="2"
+          class="childComponent"
+          title="報名表設計"
+          icon="assignment"
+          :done="step > 2"
+        >
+          <formDesign />
         </q-step>
 
-        <q-step :name="3" class="childComponent" title="預覽" icon="visibility" :done="step > 4">
-          <ActivityDetailsView></ActivityDetailsView>
+        <q-step
+          :name="3"
+          class="childComponent"
+          title="預覽"
+          icon="visibility"
+          :done="step > 4"
+        >
+          <ActivityDetailsView :preview="allChildPara.basicInfo.preview" />
         </q-step>
 
         <template v-slot:navigation>
           <q-stepper-navigation>
-            <q-btn class="btn-step" @click="$refs.stepper.next()" :label="step === 8 ? 'Finish' : 'Continue'" flat></q-btn>
-            <q-btn v-if="step > 1" flat @click="$refs.stepper.previous()" label="Back" class="btn-step" style="margin-left:5px;"></q-btn>
-            <br/>
-            <q-btn class="btn-step" style="margin-top:5px;" @click="save">儲存</q-btn>
+            <q-btn
+              class="btn-step"
+              @click="$refs.stepper.next()"
+              :label="step === 8 ? 'Finish' : 'Continue'"
+              flat
+            ></q-btn>
+            <q-btn
+              v-if="step > 1"
+              flat
+              @click="$refs.stepper.previous()"
+              label="Back"
+              class="btn-step"
+              style="margin-left: 5px"
+            ></q-btn>
+            <br />
+            <q-btn class="btn-step" style="margin-top: 5px" @click="save"
+              >儲存</q-btn
+            >
           </q-stepper-navigation>
         </template>
-
       </q-stepper>
     </div>
   </main>
 </template>
 
 <script setup>
+import Head from "../components/Head.vue";
 import { ref } from "vue";
 import { apiSaveEventData } from "@/apis/api/userRequest.ts";
 import basicInfo from "@/components/ActivityCreatePage/basicInfo.vue";
@@ -48,18 +80,28 @@ const step = ref(1);
 
 let allChildPara = {};
 
-
 function save() {
   apiSaveEventData(allChildPara.basicInfo).then((response) => {
     if (response.status == 200) {
       alert("儲存成功");
     }
   });
+  // console.log(allChildPara);
+  // factoryFn(allChildPara.basicInfo.preview);
+}
+
+function factoryFn(file) {
+  console.log(file);
+  return new Promise((resolve, reject) => {
+    resolve({
+      url: "https://localhost:5001/api/Event/dataUpload",
+      method: "POST",
+    });
+  });
 }
 
 // 取得子元件emit
-function getPara({event}) {
+function getPara({ event }) {
   allChildPara.basicInfo = event;
 }
-
 </script>
