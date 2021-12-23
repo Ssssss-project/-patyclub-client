2dba8976057967feda6c1051d0d66b86cf35739e
xiouchin32 <xiouchin32@gmail.com>
2021-12-23 10:34:37 +0800

    add pagination

--- src/views/ActivityView.vue
+++ src/views/ActivityView.vue
@@ -2,8 +2,7 @@
     <main id="ActivityView">
         <div class="container">
             <div class="activity-main">
-                <div class="head-block">
-                </div>
+                <div class="head-block"></div>
                 <div class="activity-filter">
                     <activity-filter />
                 </div>
@@ -11,8 +10,11 @@
                     <div class="event-category scrollbarCol">
                         <event-category />
                     </div>
-                    <div class="card-list scrollbarCol">
-                        <card-list />
+                    <div class="card-set">
+                        <div class="card-list scrollbarCol">
+                            <card-list />
+                        </div>
+                        <q-pagination v-model="page" color="orange-12" :min="1" :max="15" :max-pages="6" :input="true"/>
                     </div>
                 </div>
             </div>
@@ -31,9 +33,13 @@ export default {
     setup() {
         const options = ["Google", "Facebook", "Twitter", "Apple", "Oracle"];
         const model = ref(options[0]);
+        const page = ref(1)
         return {
             options,
             model,
+            page: page,
+            minPages: 1,
+            maxPages: 15,
         };
     },
     components: {
