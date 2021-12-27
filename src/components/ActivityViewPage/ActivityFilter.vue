aa79693d4b72e3617d42a98bb401acfbab1b32a7
Xiou chin MA <xiouchinma@XioudeMacBook-Air.local>
2021-12-27 19:07:02 +0800

    fix laptop card width

--- src/components/ActivityViewPage/ActivityFilter.vue
+++ src/components/ActivityViewPage/ActivityFilter.vue
@@ -1,86 +1,113 @@
 <template>
-    <div class="right-search">
-        <div class="right-btn">
-            <q-btn style="background: #eeb888; color: #402e32;border:1px solid black;" label="全部" size="sm" />
-            <q-btn style="background: #ffffff; color: #402e32 ;border:1px solid black;" label="精選" size="sm" />
-        </div>
-        <div>
-            <q-select
-                v-model="searchWords"
-                use-input
-                use-chips
-                multiple
-                borderless
-                input-debounce="0"
-                @new-value="createValue"
-                :options="filterOptions"
-                @filter="filterFn"
-                class="select-category"
-                dropdown-icon="search"
-                style="max-width:250px"
-            />
-            <div class="search-words">
-                <span v-for="val in stringOptions" :key="val">
-                    {{ val }}
-                </span>
-            </div>
-        </div>
+  <div class="right-search">
+    <div class="right-btn">
+      <q-btn
+        style="background: #eeb888; color: #402e32;border:1px solid black;"
+        label="全部"
+        size="md"
+      />
+      <q-btn
+        style="background: #ffffff; color: #402e32 ;border:1px solid black;"
+        label="精選"
+        size="md"
+      />
     </div>
-    <div class="left-search">
-        <div>
-            <label>檢視</label>
-            <q-select class="select-category" v-model="model" :options="options" borderless dense options-dense />
-        </div>
-        <div>
-            <label>排序</label>
-            <q-select class="select-category" v-model="model" :options="options" borderless dense options-dense />
-        </div>
+    <div>
+      <q-select
+        v-model="searchWords"
+        use-input
+        use-chips
+        multiple
+        borderless
+        input-debounce="0"
+        @new-value="createValue"
+        :options="filterOptions"
+        @filter="filterFn"
+        class="select-category"
+        dropdown-icon="search"
+        style="max-width:250px"
+      />
+      <div class="search-words">
+        <span
+          v-for="val in stringOptions"
+          :key="val"
+        >
+          {{ val }}
+        </span>
+      </div>
     </div>
+  </div>
+  <div class="left-search">
+    <div>
+      <label>檢視</label>
+      <q-select
+        class="select-category"
+        v-model="model"
+        :options="options"
+        borderless
+        dense
+        options-dense
+      />
+    </div>
+    <div>
+      <label>排序</label>
+      <q-select
+        class="select-category"
+        v-model="model"
+        :options="options"
+        borderless
+        dense
+        options-dense
+      />
+    </div>
+  </div>
 </template>
 
 <script>
 import { ref } from "vue";
 
 export default {
-    setup() {
-        const options = ["Google", "Facebook", "Twitter", "Apple", "Oracle"];
-        const model = ref(options[0]);
-        const stringOptions = [];
-        const filterOptions = ref(stringOptions);
-        const searchWords = ref(null);
+  setup() {
+    const options = ["Google", "Facebook", "Twitter", "Apple", "Oracle"];
+    const model = ref(options[0]);
+    const stringOptions = [];
+    const filterOptions = ref(stringOptions);
+    const searchWords = ref(null);
 
-        const createValue = (val, done) => {
-            if (!stringOptions.includes(val)) {
-                stringOptions.push(val);
-                done(val, "add-unique");
-            }
-        };
+    const createValue = (val, done) => {
+      if (!stringOptions.includes(val)) {
+        stringOptions.push(val);
+        done(val, "add-unique");
+      }
+    };
 
-        const filterFn = (val, update) => {
-            update(() => {
-                if (val === "") {
-                    filterOptions.value = stringOptions;
-                } else {
-                    const needle = val;
-                    filterOptions.value = stringOptions.filter((v) => v.indexOf(needle) > -1);
-                }
-            });
-        };
-        const inputValue = (val) => {
-            console.log(val);
-        };
+    const filterFn = (val, update) => {
+      update(() => {
+        if (val === "") {
+          filterOptions.value = stringOptions;
+        } else {
+          const needle = val;
+          filterOptions.value = stringOptions.filter(
+            (v) => v.indexOf(needle) > -1
+          );
+        }
+      });
+    };
+    const inputValue = (val) => {
+      console.log(val);
+    };
 
-        return {
-            options,
-            model,
-            stringOptions,
-            filterOptions,
-            searchWords,
-            filterFn,
-            createValue,
-            inputValue,
-        };
-    },
-    components: {},
+    return {
+      options,
+      model,
+      stringOptions,
+      filterOptions,
+      searchWords,
+      filterFn,
+      createValue,
+      inputValue,
+    };
+  },
+  components: {},
 };
 </script>
--- src/views/ActivityView.vue
+++ src/views/ActivityView.vue
@@ -1,32 +1,32 @@
 <template>
-    <main id="ActivityView">
-        <div class="container">
-            <div class="activity-main">
-                <div class="head-block"></div>
-                <div class="activity-filter">
-                    <activity-filter />
-                </div>
-                <div class="activity-information">
-                    <div class="event-category scrollbarCol">
-                        <event-category />
-                    </div>
-                    <div class="card-set">
-                        <div class="card-list scrollbarCol">
-                            <card-list />
-                        </div>
-                        <q-pagination
-                            v-model="page"
-                            color="orange-12"
-                            :min="1"
-                            :max="15"
-                            :max-pages="6"
-                            :input="true"
-                        />
-                    </div>
-                </div>
-            </div>
+  <main id="ActivityView">
+    <div class="container">
+      <div class="activity-main">
+        <div class="head-block"></div>
+        <div class="activity-filter">
+          <activity-filter />
         </div>
-    </main>
+        <div class="activity-information">
+          <div class="event-category scrollbarCol">
+            <event-category />
+          </div>
+          <div class="card-set">
+            <div class="card-list scrollbarCol">
+              <card-list />
+            </div>
+            <q-pagination
+              v-model="page"
+              color="orange-12"
+              :min="1"
+              :max="15"
+              :max-pages="6"
+              :input="true"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  </main>
 </template>
 
 <script>
@@ -37,22 +37,22 @@ import ActivityFilter from "../components/ActivityViewPage/ActivityFilter.vue";
 import { ref } from "vue";
 
 export default {
-    setup() {
-        const options = ["Google", "Facebook", "Twitter", "Apple", "Oracle"];
-        const model = ref(options[0]);
-        const page = ref(1);
-        return {
-            options,
-            model,
-            page: page,
-            minPages: 1,
-            maxPages: 15,
-        };
-    },
-    components: {
-        CardList,
-        EventCategory,
-        ActivityFilter,
-    },
+  setup() {
+    const options = ["Google", "Facebook", "Twitter", "Apple", "Oracle"];
+    const model = ref(options[0]);
+    const page = ref(1);
+    return {
+      options,
+      model,
+      page: page,
+      minPages: 1,
+      maxPages: 15,
+    };
+  },
+  components: {
+    CardList,
+    EventCategory,
+    ActivityFilter,
+  },
 };
 </script>
