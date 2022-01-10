f75e8c1e5e5834697f6e18e0bc0b7d8c2cced54c
xiouchin32 <xiouchin32@gmail.com>
2022-01-10 17:02:14 +0800

    add search words and css

--- src/components/ActivityViewPage/ActivityFilter.vue
+++ src/components/ActivityViewPage/ActivityFilter.vue
@@ -1,86 +1,64 @@
 <template>
-  <div class="right-search">
-    <div class="right-btn">
-      <q-btn
-        class="activity-all"
-        label="全部"
-        size="md"
-      />
-      <q-btn
-        class="activity-recommended"
-        label="精選"
-        size="md"
-      />
+    <div class="left-search">
+        <div class="right-btn">
+            <q-btn class="activity-all" label="全部" size="md" />
+            <q-btn class="activity-recommended" label="精選" size="md" />
+        </div>
+        <div class="search-input">
+            <q-input class="search-textfield" v-model="searchWords" @keydown.enter="submit(searchWords)" borderless>
+                <template v-slot:append>
+                    <q-icon name="search" @click="submit(searchWords)" class="cursor-pointer" />
+                </template>
+            </q-input>
+            <div class="search-words">
+                <span v-for="(val, index) in stringOptions" :key="val">
+                    <q-chip dense size="md" color="#eeb888"
+                        >{{ val }} <q-icon name="close" @click="deleteValue(index)" class="cursor-pointer"
+                    /></q-chip>
+                </span>
+            </div>
+        </div>
     </div>
-    <div>
-      <div class="col-3">
-        <q-input v-model="searchWords" @keydown.enter="submit(searchWords)">
-          <template v-slot:append>
-            <q-icon name="search" @click="submit(searchWords)" class="cursor-pointer"/>
-          </template>
-        </q-input>
-      </div>
-      <div>
-        <span
-          v-for="val in stringOptions"
-          :key="val"
-        >
-          <q-chip dense size="md">{{val}}  <q-icon name="close" @click="text = ''" class="cursor-pointer" /></q-chip>
-        </span>
-      </div>
+    <div class="right-search">
+        <div>
+            <label>檢視</label>
+            <q-select class="select-category" v-model="model" :options="options" borderless dense options-dense />
+        </div>
+        <div>
+            <label>排序</label>
+            <q-select class="select-category" v-model="model" :options="options" borderless dense options-dense />
+        </div>
     </div>
-  </div>
-  <div class="left-search">
-    <div>
-      <label>檢視</label>
-      <q-select
-        class="select-category"
-        v-model="model"
-        :options="options"
-        borderless
-        dense
-        options-dense
-      />
-    </div>
-    <div>
-      <label>排序</label>
-      <q-select
-        class="select-category"
-        v-model="model"
-        :options="options"
-        borderless
-        dense
-        options-dense
-      />
-    </div>
-  </div>
 </template>
 
 <script>
 import { ref } from "vue";
 
 export default {
-  setup() {
-    const options = ["Google", "Facebook", "Twitter", "Apple", "Oracle"];
-    const model = ref(options[0]);
-    const stringOptions = [];
-    const searchWords = ref(null);
+    setup() {
+        const options = ["Google", "Facebook", "Twitter", "Apple", "Oracle"];
+        const model = ref(options[0]);
+        const stringOptions = ref([]);
+        const searchWords = ref(null);
 
-    const submit = (refKey) => {
-      stringOptions.push(refKey)
-      searchWords.value = ""
-    }
+        const submit = (refKey) => {
+            stringOptions.value.push(refKey);
+            searchWords.value = "";
+        };
 
-    
+        const deleteValue = (index) => {
+            stringOptions.value.splice(index, 1);
+        };
 
-    return {
-      options,
-      model,
-      stringOptions,
-      searchWords,
-      submit
-    };
-  },
-  components: {},
+        return {
+            options,
+            model,
+            stringOptions,
+            searchWords,
+            deleteValue,
+            submit,
+        };
+    },
+    components: {},
 };
 </script>
