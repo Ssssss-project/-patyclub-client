c41702540faea12cc675cf90c1ba03b00b1ec491
Xiou chin MA <xiouchinma@XioudeMacBook-Air.local>
2022-02-13 10:41:48 +0800

    fix formate

--- src/components/ActivityViewPage/CardList.vue
+++ src/components/ActivityViewPage/CardList.vue
@@ -1,69 +1,77 @@
 <template>
-    <q-card
-        v-ripple
-        class="my-card cursor-pointer "
-        v-for="(data, idx) in Activity"
-        :key="idx"
-        @mouseover="data.showInfo = true"
-        @mouseleave="data.showInfo = false"
+  <q-card
+    v-ripple
+    class="my-card cursor-pointer "
+    v-for="(data, idx) in Activity"
+    :key="idx"
+    @mouseover="data.showInfo = true"
+    @mouseleave="data.showInfo = false"
+  >
+    <img :src="data.image" />
+    <q-card-section>
+      <div class="text-h6">{{ data.eventTitle }}</div>
+      <div class="text-subtitle2">{{ data.owner !== "" ? `by ${data.owner}` : "" }}</div>
+    </q-card-section>
+    <div class="timeline">
+      <span>{{ timeDiffCalc(new Date(data.eventEdDate)) }}</span>
+    </div>
+
+    <div
+      class="q-pt-none"
+      v-if="data.showInfo"
     >
-        <img :src="data.image" />
+      <div class="text-h6">{{ data.eventTitle }}</div>
+      <div class="hover-content scrollbarCol">
+        <span>{{ data.eventIntroduction }}</span>
+        <div class="text-subtitle2">{{ data.owner !== "" ? `by ${data.owner}` : "" }}</div>
+      </div>
 
-        <q-card-section>
-            <div class="text-h6">{{ data.eventTitle }}</div>
-            <div class="text-subtitle2">{{ data.owner !== "" ? `by ${data.owner}` : "" }}</div>
-        </q-card-section>
-        <div class="timeline">
-            <span>{{ timeDiffCalc(new Date(data.eventEdDate)) }}</span>
-        </div>
-
-        <div class="q-pt-none" v-if="data.showInfo">
-            <div class="text-h6">{{ data.eventTitle }}</div>
-            <div class="hover-content scrollbarCol">
-                <span>{{ data.eventIntroduction }}</span>
-                <div class="text-subtitle2">{{ data.owner !== "" ? `by ${data.owner}` : "" }}</div>
-            </div>
-
-            <div class="timeline">
-                <span>{{ timeDiffCalc(new Date(data.eventEdDate)) }}</span>
-            </div>
-        </div>
-    </q-card>
+      <div class="timeline">
+        <span>{{ timeDiffCalc(new Date(data.eventEdDate)) }}</span>
+      </div>
+    </div>
+  </q-card>
 </template>
 
 <script lang="ts">
 import { toRef } from "vue";
 
 export default {
-    name: "CardList",
-
-    setup(props: any) {
-        const Activity = toRef(props, "AllEvent");
-
-        function timeDiffCalc(dateFuture: Date) {
-            let dateNow = new Date();
-            let diffInMilliSeconds: number = Math.abs(dateFuture.getTime() - dateNow.getTime()) / 1000;
-            // calculate days
-            const days = Math.floor(diffInMilliSeconds / 86400);
-            diffInMilliSeconds -= days * 86400;
-            // calculate hours
-            const hours = Math.floor(diffInMilliSeconds / 3600) % 24;
-            diffInMilliSeconds -= hours * 3600;
-            // calculate minutes
-            const minutes = Math.floor(diffInMilliSeconds / 60) % 60;
-            diffInMilliSeconds -= minutes * 60;
-            let difference = "";
-            if (days > 0) {
-                difference += days === 1 ? `${days} day ` : `${days} days `;
-            }
-            difference += hours === 0 || hours === 1 ? `${hours} hour ` : `${hours} hours `;
-            difference += minutes === 0 || hours === 1 ? `${minutes} minutes` : `${minutes} minutes`;
-            return difference;
-        }
-        return {
-            Activity,
-            timeDiffCalc,
-        };
-    },
+  name: "CardList",
+  props: ["allEvent"],
+  setup(props: any) {
+    const Activity = toRef(props, "allEvent");
+    console.log(Activity);
+    //計算時間差距
+    function timeDiffCalc(dateFuture: Date) {
+      let dateNow = new Date();
+      let diffInMilliSeconds: number =
+        Math.abs(dateFuture.getTime() - dateNow.getTime()) / 1000;
+      // calculate days
+      const days = Math.floor(diffInMilliSeconds / 86400);
+      diffInMilliSeconds -= days * 86400;
+      // calculate hours
+      const hours = Math.floor(diffInMilliSeconds / 3600) % 24;
+      diffInMilliSeconds -= hours * 3600;
+      // calculate minutes
+      const minutes = Math.floor(diffInMilliSeconds / 60) % 60;
+      diffInMilliSeconds -= minutes * 60;
+      let difference = "";
+      if (days > 0) {
+        difference += days === 1 ? `${days} day ` : `${days} days `;
+      }
+      difference +=
+        hours === 0 || hours === 1 ? `${hours} hour ` : `${hours} hours `;
+      difference +=
+        minutes === 0 || hours === 1
+          ? `${minutes} minutes`
+          : `${minutes} minutes`;
+      return difference;
+    }
+    return {
+      Activity,
+      timeDiffCalc,
+    };
+  },
 };
 </script>
--- src/views/ActivityView.vue
+++ src/views/ActivityView.vue
@@ -1,32 +1,32 @@
 <template>
-    <main id="ActivityView">
-        <div class="container">
-            <div class="activity-main">
-                <div class="head-block"></div>
-                <div class="activity-filter">
-                    <activity-filter />
-                </div>
-                <div class="activity-information">
-                    <div class="event-category scrollbarCol">
-                        <event-category />
-                    </div>
-                    <div class="card-set">
-                        <div class="card-list scrollbarCol">
-                            <card-list :AllEvent="allEvent" />
-                        </div>
-                        <q-pagination
-                            v-model="page"
-                            color="orange-12"
-                            :min="1"
-                            :max="15"
-                            :max-pages="6"
-                            :input="true"
-                        />
-                    </div>
-                </div>
-            </div>
+  <main id="ActivityView">
+    <div class="container">
+      <div class="activity-main">
+        <div class="head-block"></div>
+        <div class="activity-filter">
+          <activity-filter />
         </div>
-    </main>
+        <div class="activity-information">
+          <div class="event-category scrollbarCol">
+            <event-category />
+          </div>
+          <div class="card-set">
+            <div class="card-list scrollbarCol">
+              <card-list :allEvent="allEvent" />
+            </div>
+            <q-pagination
+              v-model="page"
+              color="orange-12"
+              :min="1"
+              :max="15"
+              :max-pages="6"
+              :input="true"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  </main>
 </template>
 
 <script lang="ts">
@@ -37,33 +37,42 @@ import { apiGetEventWithCondition, EventType } from "../apis/api/userRequest";
 import { ref, onMounted, Ref } from "vue";
 
 export default {
-    setup() {
-        const options: string[] = ["Google", "Facebook", "Twitter", "Apple", "Oracle"];
-        const page: Ref<number> = ref(1);
-        const allEvent: Ref<EventType[]> = ref([]);
+  setup() {
+    const options: string[] = [
+      "Google",
+      "Facebook",
+      "Twitter",
+      "Apple",
+      "Oracle",
+    ];
+    const page: Ref<number> = ref(1);
+    const allEvent: Ref<EventType[]> = ref([]);
 
-        const getEventWithCondition = () => {
-            apiGetEventWithCondition({ tag: "", eventStDate: "" }).then((response: any) => {
-                allEvent.value = response.data;
-            });
-        };
-        onMounted(() => {
-            getEventWithCondition();
-        });
+    const getEventWithCondition = () => {
+      apiGetEventWithCondition({ tag: "", eventStDate: "" }).then(
+        (response: any) => {
+          console.log(response.data);
+          allEvent.value = response.data;
+        }
+      );
+    };
+    onMounted(() => {
+      getEventWithCondition();
+    });
 
-        return {
-            options,
-            page: page,
-            minPages: 1,
-            maxPages: 15,
-            allEvent,
-        };
-    },
+    return {
+      options,
+      page: page,
+      minPages: 1,
+      maxPages: 15,
+      allEvent,
+    };
+  },
 
-    components: {
-        CardList,
-        EventCategory,
-        ActivityFilter,
-    },
+  components: {
+    CardList,
+    EventCategory,
+    ActivityFilter,
+  },
 };
 </script>
