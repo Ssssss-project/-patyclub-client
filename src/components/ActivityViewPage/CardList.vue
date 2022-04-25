b1af13b013cfbc4737690f52415ba0fa0d3801b9
xiouchin32 <xiouchin32@gmail.com>
2022-04-25 09:12:04 +0800

    fix card css

--- src/components/ActivityViewPage/CardList.vue
+++ src/components/ActivityViewPage/CardList.vue
@@ -1,42 +1,39 @@
 <template>
-  <q-card
-    v-ripple
-    class="my-card cursor-pointer "
-    v-for="(data, idx) in Activity"
-    :key="idx"
-    @mouseover="data.showInfo = true"
-    @mouseleave="data.showInfo = false"
-    @click="clickCard"
-  >
-    <q-icon
-      name="favorite"
-      :class="data.isWatcher?'favorite':'unfavorite'"
-      @click="(e) => clickFavorite(e, data.id, data.userPersonnel)"
-    />
-    <img :src="data.image" />
-    <q-card-section>
-      <div class="text-h6">{{ data.eventTitle }}</div>
-      <div class="text-subtitle2">{{ data.owner !== "" ? `by ${data.owner}` : "" }}</div>
-    </q-card-section>
-    <div class="timeline">
-      <span>{{ timeDiffCalc(new Date(data.eventEdDate)) }}</span>
-    </div>
-
-    <div
-      class="q-pt-none"
-      v-if="data.showInfo"
+    <q-card
+        v-ripple
+        class="my-card cursor-pointer "
+        v-for="(data, idx) in Activity"
+        :key="idx"
+        @mouseover="data.showInfo = true"
+        @mouseleave="data.showInfo = false"
+        @click="clickCard"
     >
-      <div class="text-h6 card-title scrollbarCol">{{ data.eventTitle }}</div>
-      <div class="hover-content scrollbarCol">
-        <span>{{ data.eventIntroduction }}</span>
-        <div class="text-subtitle2">{{ data.owner !== "" ? `by ${data.owner}` : "" }}</div>
-      </div>
+        <q-icon
+            name="favorite"
+            :class="data.isWatcher ? 'favorite' : 'unfavorite'"
+            @click="(e) => clickFavorite(e, data.id, data.userPersonnel)"
+        />
+        <img :src="data.image" />
+        <q-card-section class="content">
+            <div class="text-h6">{{ data.eventTitle }}</div>
+            <div class="text-subtitle2 ">{{ data.owner !== "" ? `by ${data.owner}` : "" }}</div>
+        </q-card-section>
+        <div class="timeline">
+            <span>{{ timeDiffCalc(new Date(data.eventEdDate)) }}</span>
+        </div>
 
-      <div class="timeline">
-        <span>{{ timeDiffCalc(new Date(data.eventEdDate)) }}</span>
-      </div>
-    </div>
-  </q-card>
+        <div class="q-pt-none" v-if="data.showInfo">
+            <div class="text-h6 card-title scrollbarCol">{{ data.eventTitle }}</div>
+            <div class="hover-content scrollbarCol">
+                <span>{{ data.eventIntroduction }}</span>
+                <div class="text-subtitle2">{{ data.owner !== "" ? `by ${data.owner}` : "" }}</div>
+            </div>
+
+            <div class="timeline">
+                <span>{{ timeDiffCalc(new Date(data.eventEdDate)) }}</span>
+            </div>
+        </div>
+    </q-card>
 </template>
 
 <script lang="ts">
@@ -46,94 +43,89 @@ import { useQuasar } from "quasar";
 import { apiPutWatchEvent } from "@/apis/api/userRequest";
 
 export default {
-  name: "CardList",
-  props: ["allEvent"],
-  setup(props: any, { emit }: any) {
-    const Activity = toRef(props, "allEvent");
-    const store = useStore();
-    const $q = useQuasar();
+    name: "CardList",
+    props: ["allEvent"],
+    setup(props: any, { emit }: any) {
+        const Activity = toRef(props, "allEvent");
+        const store = useStore();
+        const $q = useQuasar();
 
-    //計算時間差距
-    function timeDiffCalc(dateFuture: Date) {
-      let dateNow = new Date();
-      let diffInMilliSeconds: number =
-        Math.abs(dateFuture.getTime() - dateNow.getTime()) / 1000;
-      // calculate days
-      const days = Math.floor(diffInMilliSeconds / 86400);
-      diffInMilliSeconds -= days * 86400;
-      // calculate hours
-      const hours = Math.floor(diffInMilliSeconds / 3600) % 24;
-      diffInMilliSeconds -= hours * 3600;
-      // calculate minutes
-      const minutes = Math.floor(diffInMilliSeconds / 60) % 60;
-      diffInMilliSeconds -= minutes * 60;
-      let difference = "";
-      if (days > 0) {
-        difference += days === 1 ? `${days} day ` : `${days} days `;
-      }
-      difference +=
-        hours === 0 || hours === 1 ? `${hours} hour ` : `${hours} hours `;
-      difference +=
-        minutes === 0 || hours === 1
-          ? `${minutes} minutes`
-          : `${minutes} minutes`;
-      return difference;
-    }
+        //計算時間差距
+        function timeDiffCalc(dateFuture: Date) {
+            let dateNow = new Date();
+            let diffInMilliSeconds: number = Math.abs(dateFuture.getTime() - dateNow.getTime()) / 1000;
+            // calculate days
+            const days = Math.floor(diffInMilliSeconds / 86400);
+            diffInMilliSeconds -= days * 86400;
+            // calculate hours
+            const hours = Math.floor(diffInMilliSeconds / 3600) % 24;
+            diffInMilliSeconds -= hours * 3600;
+            // calculate minutes
+            const minutes = Math.floor(diffInMilliSeconds / 60) % 60;
+            diffInMilliSeconds -= minutes * 60;
+            let difference = "";
+            if (days > 0) {
+                difference += days === 1 ? `${days} day ` : `${days} days `;
+            }
+            difference += hours === 0 || hours === 1 ? `${hours} hour ` : `${hours} hours `;
+            difference += minutes === 0 || hours === 1 ? `${minutes} minutes` : `${minutes} minutes`;
+            return difference;
+        }
 
-    const returnSVG = () => {
-      return "img:" + require(`@/assets/icon/heartSolid.svg`);
-    };
+        const returnSVG = () => {
+            return "img:" + require(`@/assets/icon/heartSolid.svg`);
+        };
 
-    const loginCheck = (eventID: number, userPersonnel: string) => {
-      let token = store.getters.getUserStore.sToken;
-      console.log("userPersonnel", userPersonnel);
-      if (token === "") {
-        $q.notify({
-          color: "red-5",
-          textColor: "white",
-          icon: "warning",
-          message: "請先登入!",
-        });
-      } else {
-        apiPutWatchEvent({
-          token: token,
-          eventId: eventID,
-        })
-          .then((res: any) => {
-            $q.notify({
-              icon: "done",
-              color: "positive",
-              message: res.message,
-            });
-            emit("getEventWithCondition");
-          })
-          .catch(() => {
-            $q.notify({
-              color: "red-5",
-              textColor: "white",
-              icon: "warning",
-              message: "新增失敗!",
-            });
-          });
-      }
-    };
+        const loginCheck = (eventID: number, userPersonnel: string) => {
+            let token = store.getters.getUserStore.sToken;
+            console.log("userPersonnel", userPersonnel);
+            if (token === "") {
+                $q.notify({
+                    color: "red-5",
+                    textColor: "white",
+                    icon: "warning",
+                    message: "請先登入!",
+                });
+            } else {
+                apiPutWatchEvent({
+                    token: token,
+                    eventId: eventID,
+                })
+                    .then((res: any) => {
+                        $q.notify({
+                            icon: "done",
+                            color: "positive",
+                            message: res.message,
+                        });
+                        emit("getEventWithCondition");
+                    })
+                    .catch(() => {
+                        $q.notify({
+                            color: "red-5",
+                            textColor: "white",
+                            icon: "warning",
+                            message: "新增失敗!",
+                        });
+                    });
+            }
+        };
 
-    const clickFavorite = (e: any, eventID: number, userPersonnel: string) => {
-      e.stopPropagation();
-      loginCheck(eventID, userPersonnel);
-    };
+        const clickFavorite = (e: any, eventID: number, userPersonnel: string) => {
+            e.stopPropagation();
+            loginCheck(eventID, userPersonnel);
+        };
 
-    const clickCard = () => {
-      console.log("---clickCard---");
-    };
+        const clickCard = () => {
+            console.log("---clickCard---");
+        };
 
-    return {
-      Activity,
-      timeDiffCalc,
-      returnSVG,
-      clickFavorite,
-      clickCard,
-    };
-  },
+        return {
+            Activity,
+            timeDiffCalc,
+            returnSVG,
+            clickFavorite,
+            clickCard,
+        };
+    },
 };
 </script>
