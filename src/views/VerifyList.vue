1befec7b802b2c74c492092ae6d125c9aadbfbbf
xiouchin <xiouchin32@gmail.com>
2022-07-03 22:03:03 +0800

    add search audit event

--- src/views/VerifyList.vue
+++ src/views/VerifyList.vue
@@ -12,6 +12,8 @@
           :dense="true"
           class="search"
           outlined
+          v-model="stringSearch"
+          @keydown.enter.prevent="SearchEvent"
         >
           <template v-slot:append>
             <q-icon name="search" />
@@ -24,33 +26,59 @@
           <th>活動資訊</th>
           <th>狀態</th>
         </tr>
-        <tr>
-          <td>活動活動</td>
-          <td>活動活動資訊</td>
-          <td>待審</td>
+        <tr v-if="allEvent.length===0">
+          <td colspan="3">查無活動</td>
         </tr>
-        <tr>
-          <td>活動活動</td>
-          <td>活動活動資訊</td>
-          <td>待審</td>
+        <tr
+          v-else
+          v-for="(event,index) in allEvent"
+          :key="index"
+        >
+          <td>{{event.eventTitle}}</td>
+          <td>{{event.eventStDate}}</td>
+          <td>{{event.statusDesc}}</td>
         </tr>
-        <tr>
-          <td>活動活動</td>
-          <td>活動活動資訊</td>
-          <td>待審</td>
-        </tr>
-        <tr>
-          <td>活動活動</td>
-          <td>活動活動資訊</td>
-          <td>待審</td>
-        </tr>
+
       </table>
     </div>
   </main>
 </template>
 
-<script>
+<script lang="ts">
+import { ref, Ref, onMounted } from "vue";
+import { EventType } from "@/apis/type";
+import { apiGetEventWithCondition } from "@/apis/api/userRequest";
 export default {
-  setup() {},
+  setup() {
+    const allEvent: Ref<EventType[]> = ref([]);
+    const stringSearch: Ref<string> = ref("");
+
+    const getEventWithCondition = () => {
+      apiGetEventWithCondition({
+        status: "AUDIT",
+      }).then((response: any) => {
+        allEvent.value = response.data;
+      });
+    };
+
+    const SearchEvent = () => {
+      apiGetEventWithCondition({
+        status: "AUDIT",
+        queryList: [stringSearch.value],
+      }).then((response: any) => {
+        allEvent.value = response.data;
+      });
+    };
+
+    onMounted(() => {
+      getEventWithCondition();
+    });
+
+    return {
+      allEvent,
+      stringSearch,
+      SearchEvent,
+    };
+  },
 };
 </script>
