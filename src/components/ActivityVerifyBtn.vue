0bba3b8a8968a9f769e31f5adeb69a433ae6ad42
xiouchin <xiouchin32@gmail.com>
2022-07-11 20:06:10 +0800

    add verify component

---
+++ src/components/ActivityVerifyBtn.vue
@@ -0,0 +1,29 @@
+
+<template>
+  <div class='out-side-directive'>
+    <div
+      class="hint-directive"
+      v-if="bShowImg"
+    >
+      <img
+        src='@/assets/review/warn.png'
+        alt=""
+      />
+    </div>
+    <slot></slot>
+  </div>
+</template>
+<script lang="ts">
+import { toRef } from "vue";
+export default {
+  props: ["ShowImg"],
+  setup(props: any) {
+    const bShowImg = toRef(props, "ShowImg");
+    console.log(bShowImg.value);
+    return {
+      bShowImg,
+    };
+  },
+  components: {},
+};
+</script>
